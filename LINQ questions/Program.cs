e79541d56c64b3b2c970d93ffa0950faf2c57c34
kee cha <kee_cha18@yahoo.com>
2019-12-18 10:43:10 -0600

    finished LINQ problem 1 with shalini. Made 2 classes to hold my list of words

--- LINQ questions/Program.cs
+++ LINQ questions/Program.cs
@@ -9,14 +9,14 @@ namespace LINQ_questions
     class Program
     {
         static void Main(string[] args)
-        {
-            var wordWithTh = "th";
+        {            
             List<Word> words = new WordList().GetWord();
-            List<Word> wordsWithTh = words.FindAll(w => w.title.Contains(wordWithTh));
+            List<Word> wordsWithTh = words.FindAll(w => w.title.Contains("th"));
             foreach (var newWord in wordsWithTh)
             {
                 Console.WriteLine(newWord.title);
             }
+            Console.ReadLine();
         }
     }
 }
