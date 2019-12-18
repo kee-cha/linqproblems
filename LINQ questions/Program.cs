7efc7f5f487e21a4678cd81a9db2928bea75becd
kee cha <kee_cha18@yahoo.com>
2019-12-18 11:19:26 -0600

    finish problem 2 and deleted extra unneccessary classes

--- LINQ questions/Program.cs
+++ LINQ questions/Program.cs
@@ -9,12 +9,19 @@ namespace LINQ_questions
     class Program
     {
         static void Main(string[] args)
-        {            
-            List<Word> words = new WordList().GetWord();
-            List<Word> wordsWithTh = words.FindAll(w => w.title.Contains("th"));
+        {
+            List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
+            List<string> wordsWithTh = words.FindAll(w => w.Contains("th"));
             foreach (var newWord in wordsWithTh)
             {
-                Console.WriteLine(newWord.title);
+                Console.WriteLine(newWord);
+            }
+
+            List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
+            List<string> distinctNames = names.Distinct().ToList();
+            foreach (var name in distinctNames)
+            {
+                Console.WriteLine(name);
             }
             Console.ReadLine();
         }
