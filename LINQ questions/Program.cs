2cf8d651d33e476a4dbca716a440645aa2d7140d
kee cha <kee_cha18@yahoo.com>
2019-12-18 10:33:09 -0600

    init

---
+++ LINQ questions/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_questions
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var wordWithTh = "th";
+            List<Word> words = new WordList().GetWord();
+            List<Word> wordsWithTh = words.FindAll(w => w.title.Contains(wordWithTh));
+            foreach (var newWord in wordsWithTh)
+            {
+                Console.WriteLine(newWord.title);
+            }
+        }
+    }
+}
---
+++ LINQ questions/Word.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LINQ_questions
+{
+    public class Word
+    {
+        public string title;
+    }
+}
