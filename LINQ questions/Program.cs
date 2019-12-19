dbbcafe940af5bc096cdc90b38abbd239ae077f4
kee cha <kee_cha18@yahoo.com>
2019-12-18 18:11:23 -0600

    finished question 3 and 4

--- LINQ questions/Program.cs
+++ LINQ questions/Program.cs
@@ -10,20 +10,44 @@ namespace LINQ_questions
     {
         static void Main(string[] args)
         {
+            //question 1
             List<string> words = new List<string>() { "the", "bike", "this", "it", "tenth", "mathematics" };
             List<string> wordsWithTh = words.FindAll(w => w.Contains("th"));
             foreach (var newWord in wordsWithTh)
             {
                 Console.WriteLine(newWord);
             }
-
+            //question 2
             List<string> names = new List<string>() { "Mike", "Brad", "Nevin", "Zack", "Mike" };
             List<string> distinctNames = names.Distinct().ToList();
-            foreach (var name in distinctNames)
+            foreach (var item in distinctNames)
             {
-                Console.WriteLine(name);
+                Console.WriteLine(item);
             }
+            //question 3
+            List<string> classGrades = new List<string>()
+            {
+                "80,100,92,89,65",
+                "93,81,78,84,69",
+                "73,88,83,99,64",
+                "98,100,66,74,55"
+            };
+        
+            var newClassGrade = classGrades.Select(c => c.Split(',').Select(n =>Convert.ToInt32(n)).OrderByDescending(n=>n).Take(c.Split(',').Length-1).Average()).Average();
+            Console.WriteLine(newClassGrade);
+
+            //question 4
+            string name = "Terrill";
+                var nameArry = name.ToUpper().ToArray().OrderBy(n=>n);
+                var result = nameArry.GroupBy(n => n).Select(n =>$"{n.Last()}{n.Count()}");
+            string finalResult = "";
+                foreach (var letter in result)
+                {
+                    finalResult+= letter.ToString();
+                }
+                Console.WriteLine(finalResult);
             Console.ReadLine();
+
         }
     }
 }
